c1817e6031710399121c0fade22eaab8aaf00a5a
Shiv Gowda <151723225+Shiv-61@users.noreply.github.com>
2025-12-22 21:09:42 +0530

    DB problem fixed

--- Model/db.js
+++ Model/db.js
@@ -3,7 +3,12 @@ require("dotenv").config();
 
 function connect() {
   mongoose
-    .connect(process.env.MONGO_URL)
+    .connect(process.env.MONGO_URL, {
+      serverSelectionTimeoutMS: 10000,
+      socketTimeoutMS: 45000,
+      maxPoolSize: 10,
+      minPoolSize: 2,
+    })
     .then(() => console.log("MongoDB Connected Successfully"))
     .catch((err) => console.log("DB Connection Error:", err));
 }
