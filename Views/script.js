f4b92de576e001fdaee2b3c70675175241d86c9e
Shiv Gowda <151723225+Shiv-61@users.noreply.github.com>
2025-12-23 09:03:17 +0530

    done url validation

--- Views/script.js
+++ Views/script.js
@@ -34,35 +34,47 @@ function copy_url() {
   }, 1500);
 }
 
+function isValidHttpUrl(string) {
+  try {
+    const url = new URL(string);
+    return url.protocol === "http:" || url.protocol === "https:";
+  } catch (_) {
+    return false;
+  }
+}
+
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
 
   const long_url = document.querySelector(".input-box").value;
+  if (isValidHttpUrl(long_url)) {
+    try {
+      const response = await fetch(
+        "https://url-shortner-one-jet.vercel.app/url",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            url: long_url,
+          }),
+        }
+      );
 
-  try {
-    const response = await fetch(
-      "https://url-shortner-one-jet.vercel.app/url",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          url: long_url,
-        }),
+      const data = await response.json();
+
+      if (!response.ok) {
+        console.error("Backend error:", data);
+        throw new Error(data.error || "Failed to shorten URL");
       }
-    );
 
-    const data = await response.json();
-
-    if (!response.ok) {
-      console.error("Backend error:", data);
-      throw new Error(data.error || "Failed to shorten URL");
+      document.getElementById("output").value = data.shortUrl;
+    } catch (err) {
+      console.error("ERROR:", err);
+      alert("Something went wrong");
     }
-
-    document.getElementById("output").value = data.shortUrl;
-  } catch (err) {
-    console.error("ERROR:", err);
-    alert("Something went wrong");
+  } else {
+    alert("Invalid url");
   }
 });
